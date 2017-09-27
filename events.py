04dd9373e9fd23926ca28c814cec1f577381741e
mcintyre94 <mcintyre1994@gmail.com>
2017-09-27 21:59:01 +0100

    Initial commit

---
+++ events.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import XenAPI
+import sys
+
+session = XenAPI.Session("")
+session.login_with_password("","")
+session.xenapi.event.register(["VM"]) # register for events in the pool and VM objects 
+
+vm_power_states = {}
+
+while True:
+  try:
+    events = session.xenapi.event.next() # block until a xapi event on a xapi DB object is available
+    for event in events:
+      if event['class'] == 'vm' and event['operation'] == 'mod':
+        vm = event['snapshot']
+        vm_name = vm['name_label']
+        vm_power = vm['power_state']
+        
+        if vm_name not in vm_power_states or vm_power_states[vm_name] != vm_power:
+          print("%s power state changed to %s" % (vm_name, vm_power))
+          vm_power_states[vm_name] = vm_power
+        
+  except XenAPI.Failure as e:
+    if len(e.details) > 0 and e.details[0] == 'EVENTS_LOST':
+      session.xenapi.event.unregister(["VM","pool"])
+      session.xenapi.event.register(["VM","pool"])
+    session.xenapi.logout()
